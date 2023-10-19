12cc51a39795b31e81629fe15b5cffb863e6f176
Alex <lunitik_muzik@outlook.com>
2023-10-19 22:30:14 +0200

    configured search by params and clear button state

--- src/components/Search/Search.tsx
+++ src/components/Search/Search.tsx
@@ -1,53 +1,65 @@
-import './Search.scss';
+import "./Search.scss";
 import { Autocomplete, Button, Container, TextField } from "@mui/material";
 import { useSearchParams } from "react-router-dom";
 import { useBlogs } from "../../providers/BlogsDataProvider/useBlogs";
-import { useTranslation } from 'react-i18next';
+import { useTranslation } from "react-i18next";
+import { useState } from "react";
 
 function Search() {
-    const [, setSearchParams] = useSearchParams();
-    const { t } = useTranslation("translation", { keyPrefix : "components.search"})
+    const [searchParams, setSearchParams] = useSearchParams();
+    const { t } = useTranslation("translation", {
+        keyPrefix: "components.search",
+    });
     const blogs = useBlogs();
+    const [disabled, setDisabled] = useState<boolean>(!searchParams ? true : false);
 
     const handleClearSearch = () => {
         setSearchParams();
-    }
+        setDisabled(true);
+    };
 
-    const handleSelectChange = (_event: React.SyntheticEvent, value: string, reason: string) => {
+    const handleSelectChange = (
+        _event: React.SyntheticEvent,
+        value: string,
+        reason: string
+    ) => {
         if (reason == "selectOption") {
-            const paramsObj = { "title" : value}
-            const params = new URLSearchParams(paramsObj)
+            const paramsObj = { title: value };
+            const params = new URLSearchParams(paramsObj);
             setSearchParams(params);
+            setDisabled(false);
         }
-    }
+    };
 
     return (
-       <Container className='search__container'>
-        <Autocomplete
-            freeSolo
-            id=""
-            className='search__autocomplete'
-            disableClearable
-            options={blogs.map((option) => option.title)}
-            onChange={handleSelectChange}
-            renderInput={(params) => (
-            <TextField
-                {...params}
-                label={t("search_input_label")}
-                InputProps={{
-                ...params.InputProps,
-                type: 'search',
-                }}
+        <Container className="search__container">
+            <Autocomplete
+                freeSolo
+                id=""
+                className="search__autocomplete"
+                disableClearable
+                options={blogs.map((option) => option.title)}
+                onChange={handleSelectChange}
+                renderInput={(params) => (
+                    <TextField
+                        {...params}
+                        label={t("search_input_label")}
+                        InputProps={{
+                            ...params.InputProps,
+                            type: "search",
+                        }}
+                    />
+                )}
             />
-            )}
-        />
-        <Button
-         variant="contained"
-         onClick={handleClearSearch}
-         disableElevation>
-            {t("clear")}
-        </Button>
-       </Container>
+            <Button
+                variant="contained"
+                onClick={handleClearSearch}
+                disabled={disabled}
+                disableElevation
+            >
+                {t("clear")}
+            </Button>
+        </Container>
     );
 }
 
