3b7a4cdeaa3f6079766e177d5d6b9a37cc29e7d4
Alex <lunitik_muzik@outlook.com>
2023-10-23 21:27:44 +0200

    fix firefox issue by initializing setLocale with a non-dynamic value

--- src/context/MUIWrapper.tsx
+++ src/context/MUIWrapper.tsx
@@ -1,13 +1,16 @@
 import {
-  createTheme,
-  CssBaseline,
-  PaletteMode,
-  responsiveFontSizes,
-  ThemeProvider,
-  useMediaQuery,
+    createTheme,
+    CssBaseline,
+    PaletteMode,
+    responsiveFontSizes,
+    ThemeProvider,
+    useMediaQuery,
 } from "@mui/material";
-import { createContext, useMemo, useState } from "react";
-import { MUILocaleData, supportedLocales } from "../localization/SupportedLocales";
+import { createContext, useEffect, useMemo, useState } from "react";
+import {
+    MUILocaleData,
+    supportedLocales,
+} from "../localization/SupportedLocales";
 import i18n from "../localization/i18n";
 
 /**
@@ -19,61 +22,75 @@ import i18n from "../localization/i18n";
   and initiating it with *null* would be uncomfortable :)
 */
 export const MUIWrapperContext = createContext({
-  toggleColorMode: () => {},
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  setLocale: (_locale: MUILocaleData) => {},
-  locale: supportedLocales[0], // defaults to the first item - English
+    toggleColorMode: () => {},
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    setLocale: (_locale: MUILocaleData) => {},
+    locale: supportedLocales[0], // defaults to the first item - English
 });
 
 const getCurrentLanguageIndex = (lang: string) => {
-  return supportedLocales.findIndex(x => x.lng == lang);
-}
+    return supportedLocales.findIndex((x) => x.lng == lang);
+};
 
 export default function MUIWrapper({
-  children,
+    children,
 }: {
-  children: React.ReactNode;
+    children: React.ReactNode;
 }) {
-    const prefersDarkMode = useMediaQuery("(prefers-color-scheme: dark)") ? "dark" : "light";
+    const prefersDarkMode = useMediaQuery("(prefers-color-scheme: dark)")
+        ? "dark"
+        : "light";
     const currentLocaleIndex: number = getCurrentLanguageIndex(i18n.language);
     const [mode, setMode] = useState<PaletteMode>(prefersDarkMode);
-    const [locale, setLocale] = useState<MUILocaleData>(supportedLocales[currentLocaleIndex]);
+    const [locale, setLocale] = useState<MUILocaleData>(
+        supportedLocales[0]
+    );
+
+    useEffect(() => {
+      setLocale(supportedLocales[currentLocaleIndex])
+    }, [currentLocaleIndex])
+
     const muiWrapperUtils = useMemo(
         () => ({
-        toggleColorMode: () => {
-            setMode((prevMode) => (prevMode === "light" ? "dark" : "light"));
-        },
+            toggleColorMode: () => {
+                setMode((prevMode) =>
+                    prevMode === "light" ? "dark" : "light"
+                );
+            },
         }),
         []
     );
 
-  const theme = useMemo(
-    () =>
-    responsiveFontSizes(createTheme({
-        palette: {
-          mode,
-        },
-        typography: {
-          fontFamily: [
-            'Open Sans',
-            'sans-serif'
-          ].join(','),
-        }
-      },
-      locale.muiCore)),
-    [mode, locale]
-  );
+    const theme = useMemo(
+        () =>
+            responsiveFontSizes(
+                createTheme(
+                    {
+                        palette: {
+                            mode,
+                        },
+                        typography: {
+                            fontFamily: ["Open Sans", "sans-serif"].join(","),
+                        },
+                    },
+                    locale.muiCore
+                )
+            ),
+        [mode, locale.muiCore]
+    );
 
-  return (
-    <MUIWrapperContext.Provider value={{
-        toggleColorMode: muiWrapperUtils.toggleColorMode,
-        locale,
-        setLocale
-      }}>
-      <ThemeProvider theme={theme}>
-        <CssBaseline enableColorScheme />
-        {children}
-      </ThemeProvider>
-    </MUIWrapperContext.Provider>
-  );
+    return (
+        <MUIWrapperContext.Provider
+            value={{
+                toggleColorMode: muiWrapperUtils.toggleColorMode,
+                locale,
+                setLocale,
+            }}
+        >
+            <ThemeProvider theme={theme}>
+                <CssBaseline enableColorScheme />
+                {children}
+            </ThemeProvider>
+        </MUIWrapperContext.Provider>
+    );
 }
